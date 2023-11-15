f0ca9e8d39a0d4892e1e9b413aba7750c29d90ea
adivea <adela@fedarch.org>
2023-11-15 15:25:07 +0100

    missing data discovered and explored, wrong timestamp fixed, data ORed and regenerated

--- LoadShelters.R
+++ LoadShelters.R
@@ -1,8 +1,11 @@
 ### The Shelter data processing from FAIMS modules
 
 ## This scripts helps import, project and combine data
-## from FAIMS2.6 modules online and local instatiations
-## It then loads OR streamlined data (line 72) and visualizes it
+## from FAIMS2.6 modules online and local instantiations
+## Upon careful merge (wrong order distorts timestamp)
+## we export to OR, streamlined multivalued columns and bring back for visualisation
+
+# COnstraints: dataset lacks 20 points from 10 May !!!
 
 # libraries
 library(sf)
@@ -12,71 +15,79 @@ library(tidyverse)
 ### CREATE DATA FROM FAIMS SERVER
 
 # To load shp or geojson from FAIMS
-sh23 <- read_sf("C:/Users/adela/Documents/Professional/Projects/AarhusShelters/data/AarhusShelters20230515Localserver/Shelter.shp")
+sh23_local <- read_sf("C:/Users/adela/Documents/Professional/Projects/AarhusShelters/data/AarhusShelters20230515Localserver/Shelter.shp")
 
 # Data lacks projection so project to UTM32N to faciliate analysis
-st_crs(sh23) <- 32632
+st_crs(sh23_local) <- 32632
 
 # Load data from online server and process similarly 
 sh23_online <- read_sf("data/shelters0507.geojson")
 
-# Merge the two datasets
-colnames(sh23)==colnames(sh23_online)
-sh23 <- rbind(sh23, sh23_online)
+# Check timeline and authorship
+library(lubridate)
+
+sh23_online %>% 
+  #filter(createdBy == "Lise Søndergaard Jensen") %>% 
+  mutate(day = as_date(createdAt0)) %>% 
+  group_by(day, createdBy) %>% 
+  tally()
+
+sh23_local %>% 
+  #filter(createdBy == "Lise Søndergaard Jensen") %>% 
+  mutate(day = as_date(createdAt0)) %>% 
+  group_by(day, createdBy) %>% 
+  tally()
+
+
+##############
+# Merge the two datasets (Careful! timestamp gets messed up)
+colnames(sh23_local)==colnames(sh23_online) 
+shelters23 <- rbind(sh23_online, sh23_local)
+
+shelters23 %>% 
+  mutate(day = as_date(createdAt0)) %>% 
+  group_by(day, createdBy) %>% 
+  tally()
+
+shelters23$createdAt0
+
+mapview(shelters23)
+
 
 # View projected data in interactive map
-mapview(sh23, cex = "Accuracy", name = "identifier" )
+mapview(shelters23, cex = "Accuracy", name = "identifier" )
 
-# How many total records?
-nrow(sh23)
 
 # What's the GPS accuracy? Beware annotations have turned some values into 'characters'
-head(sh23$Accuracy)
-summary(as.numeric(sh23$Accuracy))
+head(shelters23$Accuracy)
+summary(as.numeric(shelters23$Accuracy))
 
 # What's the accessibility?
-mapview(sh23, zcol = "Accessibl0")
+mapview(shelters23, zcol = "Accessibl0")
 
 # the accessibility colum needs to be cleaned up in OpenRefine
-
-# Write data to a shapefile
-st_write(sh23, "data/shelters0530.shp") # combined data
-st_write(sh23, "data/shelters0530.geojson") # all 143 recs
-saveRDS(sh23,"data/shelters.rds")
-
-
-### LOAD STREAMLINED 2023 DATA FOR SHELTERS
-
-# read it from local data/ folder
-shelter <- read_rds("data/shelters.rds")
-
-# convert it to shapefile if you need it in other software, but beware, names get truncated
-#?st_write()
-# st_write(shelter, "../data/shelters.shp")
-
 
 # view the data
 mapview(shelter, cex = "Accuracy")
 
 mapview(shelter, zcol = "LanduseAr0")
-View(shelter)
-
 
 # Export as csv
 
-shelter %>% 
+shelters23 %>% 
   st_drop_geometry() %>% 
   write_csv("data/shelters.csv")
+
 
 ###################################################
 
 ### Read REFINED csv back after OR processing
 
-shelters <- read_csv("data/sheltersS23.csv")
-head(shelters)
-colnames(shelters)
+sh23 <- read_csv("data/SheltersCleaned20231115.csv")
+head(sh23)
+colnames(sh23)
 
-sh23 <- shelters %>% 
+sh23 <- sh23 %>% 
   st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326)
 
 mapview(sh23, zcol = "Accessible")
@@ -85,6 +96,7 @@ mapview(sh23, zcol = "FeatureType")
 
 
 unique(sh23$FeatureType)
+
 library(tmap)
 
 sh_typesclean <- sh23 %>% 
@@ -111,3 +123,15 @@ tm_shape(sh_typesclean)+
 sh23 %>% 
   filter(Accessible == "Open") %>% 
   mapview()
+
+### SAVE STREAMLINED 2023 DATA FOR SHELTERS
+
+
+# Write data to a shapefile (FIRST OPENREFINE)
+saveRDS(sh23,"data/shelters23.rds")
+st_write(sh23, "data/shelters23.shp", append =F) # combined data
+st_write(sh23, "data/shelters23.geojson") # all 143 recs
+
+# Reload and check
+shelter <- read_rds("data/shelters.rds")
+sort(shelter$identifier)
