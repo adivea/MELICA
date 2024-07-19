41992003a433979bc3a6a49194a17a0c06ad36c9
aiswary-a <aisroy.23@gmail.com>
2024-07-19 23:03:26 +0200

    fixed bug, added more substitutions, incorporated new addresses

--- 09_SRCrossReference.R
+++ 09_SRCrossReference.R
@@ -7,21 +7,36 @@ library(pacman) # for easier managing of packages
 pacman::p_load(tidyverse, stringr, sf)
 
 # loading in CSVs with SR addresses, currently only for testing
-addressSR <- read_csv("Own Git Stuff/SR_overview.csv") # manually aggregated SR addresses
+addressSR <- read_csv("Own Git Stuff/SR_overview_test.csv") # manually aggregated SR addresses
 bbr <- read_csv("MELICA/output_data/SK_bbr_oc_addresses.csv") # data from BBR
 
 # removal of duplicate + empty columns
 addressSR <- subset(addressSR, select = -c(longitude, latitude))
 
-# all lowercase addresses
+# removing of whitespaces and all lowercase addresses
 addressSR$address <- str_trim(tolower(addressSR$address))
 bbr$oc_formatted <- str_trim(tolower(bbr$oc_formatted))
 
-# replacing strings to avoid any inconsistencies in spelling across sources
+# replacing strings to avoid any inconsistencies in spelling + spacing across sources
+## preliminary cleaning of dataframes, to be synthesised later
 addressSR$address <- gsub("å", "aa", addressSR$address)
 bbr$oc_formatted <- gsub("å", "aa", bbr$oc_formatted)
 
+bbr$oc_formatted <- gsub("é", "e", bbr$oc_formatted)
+bbr$oc_formatted <- gsub("ü", "u", bbr$oc_formatted)
+
+addressSR$address <- gsub("i. huitfeldt", "ivar huitfeldt", addressSR$address)
+addressSR$address <- gsub("nordre", "ndr", addressSR$address)
+addressSR$address <- gsub("dr. holstvej", "doktor holstsvej", addressSR$address)
+addressSR$address <- gsub("chr.", "christian", addressSR$address)
 addressSR$address <- gsub("rudolf", "rudolph", addressSR$address)
+addressSR$address <- gsub("rud.", "rudolph", addressSR$address)
+addressSR$address <- gsub("p. muller", "paludan muller", addressSR$address)
+addressSR$address <- gsub("j. knudsen", "jacob knudsen", addressSR$address)
+addressSR$address <- gsub("dr. margrethe", "dronning margrethe", addressSR$address)
+
+addressSR$address <- gsub("(?<=[a-zA-Z])-(?=[a-zA-Z])", "", addressSR$address, perl = TRUE)
+bbr$oc_formatted <- gsub("(?<=[a-zA-Z])-(?=[a-zA-Z])", "", bbr$oc_formatted, perl = TRUE)
 
 addressSR$address <- str_replace_all(addressSR$address, "\\s+", "")
 bbr$oc_formatted = str_replace_all(bbr$oc_formatted, "\\s+", "")
@@ -80,18 +95,29 @@ unmatched_bbr <- anti_join(bbr, addressSR, by = c("oc_formatted" = "address"))
 # Calculate the row difference
 row_diff <- nrow(unmatched_addressSR) - nrow(unmatched_bbr)
 
-# Adding NA rows to the shorter dataframe
+# Adding NA rows to the shorter df
 if (row_diff > 0) {
   # unmatched_addressSR is longer
-  extra_rows <- data.frame(bbr_leftovers = rep(NA, row_diff))
+  extra_rows <- data.frame(oc_formatted = rep(NA, row_diff))
   unmatched_bbr <- bind_rows(unmatched_bbr, extra_rows)
 } else {
   # unmatched_bbr is longer
-  extra_rows <- data.frame(addressSR_leftovers = rep(NA, abs(row_diff)))
+  extra_rows <- data.frame(address = rep(NA, abs(row_diff)))
   unmatched_addressSR <- bind_rows(unmatched_addressSR, extra_rows)
 }
 
-# gluing columns with unmatched addresses together
-checkAddressUnmatched <- subset(checkAddressUnmatched, select = c(address, oc_formatted))
+# Combine the two data frames into one with two columns
+checkAddressUnmatched <- data.frame(
+  addressSR = unmatched_addressSR$address,
+  addressBBR = unmatched_bbr$oc_formatted
+)
 
 # - - - - Spatially join identified BBR points with BBR building footprints (?)
+## geojson open with sf package + create brief report on usability + relevance
+
+geojson_streets <- "https://webkort.aarhuskommune.dk/spatialmap?page=get_geojson_opendata&datasource=vjmdt_tot"
+
+data_geo <- sf::st_read(geojson_streets, quiet = TRUE)
+
+spatial_data$name <- tolower(gsub("[[:punct:][:space:]]", "", spatial_data$name))
+csv_data$name <- tolower(gsub("[[:punct:][:space:]]", "", csv_data$name))
